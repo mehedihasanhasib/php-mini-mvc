8197d8c94519debfbdce6a1bd86c2304c263674d
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2025-02-07 13:13:22 +0600

    added composer autoload

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ session_start();
 define('BASE_PATH', dirname(__DIR__));
 require_once BASE_PATH . "/app/Helpers/functions.php";
 
-require_once BASE_PATH . "/public/autoload.php";
+// require_once BASE_PATH . "/public/autoload.php";
+require_once BASE_PATH . "/vendor/autoload.php";
 
 require_once BASE_PATH . "/bootstrap/app.php";
